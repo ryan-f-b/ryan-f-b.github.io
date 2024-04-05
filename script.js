76ed64deeeeca1df3bb6c43da23d89e3213e2f21
ryan-f-b <98054551+ryan-f-b@users.noreply.github.com>
2024-04-05 22:05:00 +0100

    added hover opacity effect on images using JS

---
+++ script.js
@@ -0,0 +1,26 @@
+alert('Yes Kiiiid');
+
+let project = document.getElementsByClassName('projectImage')[0];
+let project1 = document.getElementsByClassName('projectImage')[1];
+let project2 = document.getElementsByClassName('projectImage')[2];
+let project3 = document.getElementsByClassName('projectImage')[3];
+let project4 = document.getElementsByClassName('projectImage')[4];
+
+let reduceOpacity = function(event) {
+    event.target.style.opacity = '0.5';
+}
+
+let restoreOpacity = (event) => {
+    event.target.style.opacity = '1';
+}
+    
+project.onmouseover = reduceOpacity;
+project.onmouseout = restoreOpacity;
+project1.onmouseover = reduceOpacity;
+project1.onmouseout = restoreOpacity;
+project2.onmouseover = reduceOpacity;
+project2.onmouseout = restoreOpacity;
+project3.onmouseover = reduceOpacity;
+project3.onmouseout = restoreOpacity;
+project4.onmouseover = reduceOpacity;
+project4.onmouseout = restoreOpacity;
